2dfe70c891453073416b72332b4d2aa66c7ce604
penny <pennyjlynette@gmail.com>
2024-09-04 13:15:23 -0400

    add login animation

--- app/screens/LoginScreen.jsx
+++ app/screens/LoginScreen.jsx
@@ -4,13 +4,11 @@ import { SafeAreaFrameContext, SafeAreaView } from 'react-native-safe-area-conte
 import { TouchableOpacity } from 'react-native'
 import {ArrowLeftIcon} from 'react-native-heroicons/solid'
 import { router } from 'expo-router';
-import LottieView from 'lottie-react-native';
+import LottieView from 'lottie-react-native'; 
 
 const {width, height} = Dimensions.get('window');
 
-
 const LoginScreen = () => {
-
   return (
     <View className="flex-1 bg-white" style={{backgroundColor: '#212434'}}> 
       <SafeAreaView className="flex">
@@ -19,13 +17,16 @@ const LoginScreen = () => {
               <ArrowLeftIcon size="25" color="black"/>
             </TouchableOpacity>
           </View>
+          <View className="justify-center "> 
+          <Text className="text-pink font-bold text-2xl">Login</Text>
+          </View>
           <View className="my-20 flex-1 justify-center">
           <LottieView source={require('../../assets/animations/login.json')}
-                             style={{width: 350, height: 350, alignSelf: 'center', justifyContent: 'center'}}
-                             autoPlay
-                             loop
-                            />
-          </View>
+            style={{width: 350, height: 350, alignSelf: 'center', justifyContent: 'center'}}
+            autoPlay
+            loop
+          />
+      </View>
       </SafeAreaView>
       <View className="flex-1 bg-purple px-8 pt-8" style={{borderTopLeftRadius: 50, borderTopRightRadius: 50}}>
         <View className="form space-y-2">
